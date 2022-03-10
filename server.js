dd581a30d2e4ac15b8109664fa967b7fbb99e6e8
emmanil <emma.nilsson.567@gmail.com>
2022-03-10 09:48:38 +0100

    emma

--- server.js
+++ server.js
@@ -44,3 +44,27 @@ function getHash(password){ // utility
     let hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`)
     return hash
 }
+
+//query allt som finns i restaurants
+server.get('/group1_foodcourt/restaurants', async (request, response)=>{
+    let result = await db.all("SELECT * FROM restaurants WHERE restaurant_id = ?", [request.params.id])
+    response.json(result)
+  })
+  
+  //query skapa en ny restaurang
+  server.post('/group1_foodcourt/restaurants/crate', async (request, response)=>{
+    let result = await db.run("INSERT INTO restaurants (restaurant_id, name, resturant_adress, type_of_cuisine) VALUES(?) ", [request.body.name])
+    response.json(result)
+  })
+  
+  //query uppdatera en restaurang
+  server.put('/group1_foodcourt/restaurants/:restaurant_id', async (request, response)=>{
+    let result = await db.run("UPDATE restaurants SET name = ?, resturant_adress = ?, type_of_cuisine = ? WHERE restaurant_id = ?", [request.body.name, request.body.resturant_adress, request.body.type_of_cuisine, request.params.restaurant_id])
+    response.json(result)
+  })
+  
+  //query delete en restaurant
+  server.delete('/group1_foodcourt/restaurants/:restaurant_id', async (request, response)=>{
+    let result = await db.run("DELETE FROM restaurants WHERE restaurant_id = ?", [request.params.restaurant_id])
+    response.json(result)
+  })
