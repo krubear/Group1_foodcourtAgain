1cdc954556b57b0264f088726a60585516453b8b
emmanil <emma.nilsson.567@gmail.com>
2022-03-10 11:34:53 +0100

    emma

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express')
 const server = express()
 server.use(express.json()) // request json body
 
+/*
+//import routers
+const resturantsRoute = require("./routes/resturantsREST")//import router file
+server.use("/resturants", resturantsRoute)
+*/
+
 // register our own little custome middleware
 
-server.use((request, response, next)=>{
-    response.setHeader('X-Created-by', 'Group1')
-    next()
+server.use((request, response, next) => {
+  response.setHeader('X-Created-by', 'Group1')
+  next()
 })
 
 // register session middleware
@@ -21,50 +27,29 @@ server.use(session({
 
 
 // Start server
-server.listen(3000, ()=>{
-    console.log('Server running at http://localhost:3000/data')
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/grupp1_OOP2')
 })
 
 const Database = require('sqlite-async')
-let db 
+let db
 Database.open('./database/foodcourt.db')
-.then(d=>{
+  .then(d => {
     db = d
     console.log(db)
-})
-.catch(err=>{
+  })
+  .catch(err => {
     console.error(err)
-})
+  })
 
 // Crypto
 
 const crypto = require("crypto")
+const { ServerResponse } = require('http')
 const salt = "paraplane".toString('hex')
-function getHash(password){ // utility
-    let hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`)
-    return hash
+function getHash(password) { // utility
+  let hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`)
+  return hash
 }
 
-//query allt som finns i restaurants
-server.get('/group1_foodcourt/restaurants', async (request, response)=>{
-    let result = await db.all("SELECT * FROM restaurants WHERE restaurant_id = ?", [request.params.id])
-    response.json(result)
-  })
-  
-  //query skapa en ny restaurang
-  server.post('/group1_foodcourt/restaurants/crate', async (request, response)=>{
-    let result = await db.run("INSERT INTO restaurants (restaurant_id, name, resturant_adress, type_of_cuisine) VALUES(?) ", [request.body.name])
-    response.json(result)
-  })
-  
-  //query uppdatera en restaurang
-  server.put('/group1_foodcourt/restaurants/:restaurant_id', async (request, response)=>{
-    let result = await db.run("UPDATE restaurants SET name = ?, resturant_adress = ?, type_of_cuisine = ? WHERE restaurant_id = ?", [request.body.name, request.body.resturant_adress, request.body.type_of_cuisine, request.params.restaurant_id])
-    response.json(result)
-  })
-  
-  //query delete en restaurant
-  server.delete('/group1_foodcourt/restaurants/:restaurant_id', async (request, response)=>{
-    let result = await db.run("DELETE FROM restaurants WHERE restaurant_id = ?", [request.params.restaurant_id])
-    response.json(result)
-  })
+
