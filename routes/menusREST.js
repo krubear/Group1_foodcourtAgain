fb1688f2145671ff53bad26e171b2a1519ec1a35
Jonatan <jonatan@Victorias-MacBook-Air.local>
2022-03-14 09:56:26 +0100

    boh

--- routes/menusREST.js
+++ routes/menusREST.js
@@ -1,17 +1,19 @@
 //menus
 
-server.get('/data/menus', async(request, response)=>{
+const httpPath = '/data/menus'
+
+router.get(httpPath, async(request, response)=>{
     let result = await db.all("SELECT * FROM menus")
     respone.json(result)
   })
   
-  server.get('/data/menus', async(request, response)=>{
+  router.get(httpPath, async(request, response)=>{
     let result = await db.all("SELECT * FROM menus WHERE restaurant_id = ?", [request.params.restaurant_id])
     response.json(result)
   
   })
   
-  server.post('/data/menus', async(request, response)=>{
+  router.post(httpPath, async(request, response)=>{
     let result = await db.run("INSERT INTO menus (restaurant_id, menu_item_id) VALUES(?, ?) ")
     respone.json(result)
   })
