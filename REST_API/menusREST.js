1c8d92a3dd5b44a413263d6d2f20d735868db9d0
Jonatan <jonatan@Victorias-MacBook-Air.local>
2022-03-14 10:12:44 +0100

    ye

--- REST_API/menusREST.js
+++ REST_API/menusREST.js
@@ -1,19 +1,19 @@
 const httpPath = '/data/menus'
 
 module.exports = function(server, db){
-    
-    server.get('/data/menus', async(request, response)=>{
+
+    server.get(httpPath, async(request, response)=>{
         let result = await db.all("SELECT * FROM menus")
         respone.json(result)
       })
       
-      server.get('/data/menus', async(request, response)=>{
+      server.get(httpPath, async(request, response)=>{
         let result = await db.all("SELECT * FROM menus WHERE restaurant_id = ?", [request.params.restaurant_id])
         response.json(result)
       
       })
       
-      server.post('/data/menus', async(request, response)=>{
+      server.post(httpPath, async(request, response)=>{
         let result = await db.run("INSERT INTO menus (restaurant_id, menu_item_id) VALUES(?, ?) ")
         respone.json(result)
       })
