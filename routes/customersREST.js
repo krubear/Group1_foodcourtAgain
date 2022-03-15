8056c1018c2acac541ac2360381122d0d45f5e2d
emmanil <emma.nilsson.567@gmail.com>
2022-03-15 13:50:17 +0100

    emma 20220315

--- routes/customersREST.js
+++ routes/customersREST.js
@@ -12,33 +12,45 @@ Database.open('./database/foodcourt.db')
 })
 
 
-router.get('/', async(request, response)=>{
-    let result = await db.all("SELECT * FROM customers")
-
-    return response.json(result)
-})
-
-router.get('/:customer_id', async(request, response)=>{
-    let result = await db.all("SELECT * FROM customers WHERE customer_id = ?", [request.params.customer_id])
-
-    return response.json(result)
-})
-
-router.post('/', async(request, response)=>{
-    let result = await db.run("INSERT INTO customers (firstname, lastname, adress) VALUES(?, ?, ?)", [request.body.firstname, request.body.lastname, request.body.adress])
-    return response.json(result)
-})
-
-router.put('/:customer_id', async(request, response)=>{
-
-    let result = await db.run("UPDATE customers SET firstname = ?, lastname = ?, adress = ? WHERE customer_id = ?", [request.body.fistname, request.body.lastname, request.body.adress, request.params.customer_id])
-    return response.json(result)
-})
-
-router.delete('/:customer_id', async(request, response)=>{
-
-    let result = await db.run("DELETE FROM customers WHERE customer_id = ?", [request.params.customer_id])
-    return response.json(result)
-})
-
-module.exports = router
+//hitta en restaurang som finns i restaurants
+router.get('/:restaurant_id', async (request, response) => {
+    let result = await db.all("SELECT * FROM restaurants WHERE restaurant_id = ?", [request.params.restaurant_id])
+  
+    response.json(result)
+  })
+  
+  //se alla restauranger som finns i restaurants
+  router.get('/', async (request, response) => {
+    let result = await db.all("SELECT * FROM restaurants")
+  
+    response.json(result)
+  })
+  
+  
+  //skapa en ny restaurang 
+  router.post('/', async (request, response) => {
+    let result = await db.run("INSERT INTO restaurants (name, restaurant_adress, type_of_cuisine) VALUES(?, ?, ?) ", [request.body.name, request.body.restaurant_adress, request.body.type_of_cuisine])
+  
+    response.json(result)
+  })
+  
+  
+  //uppdatera en restaurang
+  router.put('/:restaurant_id', async (request, response) => {
+    let result = await db.run("UPDATE restaurants SET name = ?, restaurant_adress = ?, type_of_cuisine = ? WHERE restaurant_id = ?", [request.body.name, request.body.restaurant_adress, request.body.type_of_cuisine, request.params.restaurant_id])
+  
+    response.json(result)
+  })
+  
+  
+  //delete en restaurant
+  router.delete('/:restaurant_id', async (request, response) => {
+    let result
+    if (request.params.restaurant_id === request.body.restaurant_id) {
+      result = await db.run("DELETE FROM restaurants WHERE restaurant_id = ?", [request.params.restaurant_id])
+    } else {
+      response.json("Incorrect id in params or body.")
+    }
+    response.json(result)
+  })
+  module.exports = router
