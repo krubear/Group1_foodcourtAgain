20be5323eb997a44a7748dd9fe94009cd29c8502
Liha86 <sanjin.elfynd@gmail.com>
2022-03-15 10:27:48 +0100

    Update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.use("/menu_items", require('./routes/menu_items_REST')) // rout defineras
 
 // register our own little custome middleware
 
-server.use((request, response, next)=>{
+server.use((request, response, next) => {
     response.setHeader('X-Created-by', 'Group1')
     next()
 })
@@ -16,35 +16,38 @@ server.use((request, response, next)=>{
 // register session middleware
 const session = require('express-session')
 server.use(session({
-  secret: '12093h0pih23r0iholpedwrioj',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // CHANGE TO true WHEN GOING LIVE!!!! preferable using an environmental variable
+    secret: '12093h0pih23r0iholpedwrioj',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false
+    } // CHANGE TO true WHEN GOING LIVE!!!! preferable using an environmental variable
 }))
 
 
 
 // Start server
-server.listen(3000, ()=>{
-    console.log('Server running at http://localhost:3000/menu_items')
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/data/menu_items')
 })
 
 const Database = require('sqlite-async')
-let db 
+let db
 Database.open('./database/foodcourt.db')
-.then(d=>{
-    db = d
-    console.log(db)
-})
-.catch(err=>{
-    console.error(err)
-})
+    .then(d => {
+        db = d
+        console.log(db)
+    })
+    .catch(err => {
+        console.error(err)
+    })
 
 // Crypto
 
 const crypto = require("crypto")
 const salt = "paraplane".toString('hex')
-function getHash(password){ // utility
+
+function getHash(password) { // utility
     let hash = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`)
     return hash
 }
