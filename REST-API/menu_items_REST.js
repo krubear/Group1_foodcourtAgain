3d15bd6c234335d921a48872704f7a07cf40a6a6
Liha86 <sanjin.elfynd@gmail.com>
2022-03-16 14:03:01 +0100

    s

--- REST-API/menu_items_REST.js
+++ REST-API/menu_items_REST.js
@@ -39,7 +39,7 @@ module.exports = function (server, db) {
       })
       response.json({
         result,
-        message: "Menuitem updated"
+        message: "Menuitem added"
       })
 
     }
@@ -62,7 +62,7 @@ module.exports = function (server, db) {
 
       response.json({
         result,
-        message: "Menuitem added"
+        message: "Menuitem updated"
       })
 
     }
