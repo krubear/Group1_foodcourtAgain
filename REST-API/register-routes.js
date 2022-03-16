55aa4ce11b08507b6015b8f26fc2e66fcb8fc4f7
Liha86 <sanjin.elfynd@gmail.com>
2022-03-16 11:53:12 +0100

    test

--- REST-API/register-routes.js
+++ REST-API/register-routes.js
@@ -3,11 +3,8 @@ const customersREST = require("./customersREST")
 const customRoutes = require('./custom-routes')
 const usersREST = require("./usersREST")
 const loginREST = require('./loginREST')
-<<<<<<< HEAD
-const menuitemsREST = require ("./menu_items_REST")
-=======
 const menus = require('./menusREST')
->>>>>>> 57fabcaf8d24bc9e448995d492e7f27901abf613
+const menuitemsREST = require('./menu_items_REST')
 
 module.exports = function(server, db){
 
@@ -16,9 +13,6 @@ module.exports = function(server, db){
     customRoutes(server, db)
     usersREST(server, db)
     loginREST(server, db)
-<<<<<<< HEAD
     menuitemsREST(server,db)
-=======
     menus(server, db)
->>>>>>> 57fabcaf8d24bc9e448995d492e7f27901abf613
 }
